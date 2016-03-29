c27e558d1180143fccf26f0c267c54b4163d7587
Tony Edwards <tony@plymouthsoftware.com>
2016-03-29 18:18:09 +0100

    Requested Changes

--- index.php
+++ index.php
@@ -2,20 +2,11 @@
 
 <div class="blog-wrapper">
 
-  <h1>Blog</h1>
+  <h1>Audio &amp; Video</h1>
 
   <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
     <div class="blog-item">
-      <!-- Blog image here -->
-      <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-      <div class ="byline"><?php the_time('l F d, Y'); ?></a></div>
-      <?php
-          if ( has_post_thumbnail() ) {
-              the_post_thumbnail('blog-thumb');
-          }
-      ?>
-
       <?php the_content(); ?>
     </div>
 
